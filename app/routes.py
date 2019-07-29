d4c7ec88bea118145611b15f4b785a6527c34ccf
Carlos Genis <carlosgenis06@gmail.com>
2019-07-29 19:14:40 +0000

    Setting up the base

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+@app.route('/funny')
+def funny():
+    return render_template("funny.html")
